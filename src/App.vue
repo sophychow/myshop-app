01b8fd76ae68639d88c3243be12dcaf6d4e68842
jo.huichow <jo.huichow@163.com>
2019-03-21 13:37:43 +0800

    init myshop

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <router-view />
     <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
 </template>
 
 <script>
-  import {mapActions} from 'vuex'
-  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 
-  export default {
-
-    mounted () {
-      // this.$store.dispatch('getAddress')
-      this.getAddress()
-      this.getUserInfo()
-    },
-
-    methods: {
-      ...mapActions(['getAddress', 'getUserInfo'])
-    },
-
-    components: {
-      FooterGuide
-    }
+export default {
+  components: {
+    FooterGuide
   }
+}
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .app
-    width 100%
-    height 100%
-    background #f5f5f5
 </style>
