3e0e789fbbcec038d74434b25c959b18407bcde5
jo.huichow <jo.huichow@163.com>
2019-03-30 09:53:39 +0800

    1、路由组件懒加载和图片懒加载 2、路由跳转replace

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Msite from '../pages/Msite/Msite.vue'
+
+// import Msite from '../pages/Msite/Msite.vue'
+const Msite = () => import('../pages/Msite/Msite.vue') //路由组件懒加载，代买分割，按需加载缓存
 import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
+
 import Login from '../pages/Login/Login.vue'
 
 import Shop from '../pages/Shop/Shop.vue'
 import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods'
 import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings'
 import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo'
+import VueLazyload from 'vue-lazyload'
+import loading from '../common/loading.gif'
+
+// 图片懒加载
+Vue.use(VueLazyload, {
+  loading
+})
 
 Vue.use(VueRouter)
 
